c3e01fc91c066fc80155944cf19b7d711e062808
GermaValid <georgekolovatov@yandex.ru>
2026-04-10 23:08:08 +0300

    LR2-Commit

--- src/main/java/ru/bmstu/iu3/service/MenuService.java
+++ src/main/java/ru/bmstu/iu3/service/MenuService.java
@@ -1,23 +1,48 @@
 package ru.bmstu.iu3.service;
 
-import ru.bmstu.iu3.menu.Dish;
+import ru.bmstu.iu3.exception.ValidationException;
+import ru.bmstu.iu3.menu.DefaultDishFactory;
+import ru.bmstu.iu3.menu.DishFactory;
 import ru.bmstu.iu3.menu.Menu;
+import ru.bmstu.iu3.menu.MenuCsvLoader;
+import ru.bmstu.iu3.menu.MenuUnitOfWork;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MenuService {
     private final Menu menu;
+    private final DishFactory dishFactory;
 
     public MenuService() {
         this.menu = new Menu();
+        this.dishFactory = new DefaultDishFactory();
         initializeMenu();
     }
 
     private void initializeMenu() {
-        // Инициализация меню (перенесено из CafeService)
-        // т.к. проект маленький, то нет смысла делать отдельный класс для загрузки меню из файла
-        // в реальной жизни была бы бд 
-        menu.addDish(new Dish("Кофе", 100, "Горячий и ароматный кофе"));
-        menu.addDish(new Dish("Чай", 50, "Прохладный чай"));
-        menu.addDish(new Dish("Пирожное", 200, "Вкусное пирожное"));
+        MenuUnitOfWork uow = new MenuUnitOfWork(menu, dishFactory);
+        try (InputStream in = MenuService.class.getResourceAsStream("/data/menu.csv")) {
+            if (in != null) {
+                try {
+                    MenuCsvLoader.loadInto(in, uow);
+                } catch (ValidationException e) {
+                    seedDefaultMenu(uow);
+                }
+            } else {
+                seedDefaultMenu(uow);
+            }
+        } catch (IOException e) {
+            uow.rollback();
+            seedDefaultMenu(uow);
+        }
+        uow.commit();
+    }
+
+    private static void seedDefaultMenu(MenuUnitOfWork uow) {
+        uow.registerNew("Кофе", 100, "Горячий и ароматный кофе");
+        uow.registerNew("Чай", 50, "Прохладный чай");
+        uow.registerNew("Пирожное", 200, "Вкусное пирожное");
     }
 
     public Menu getMenu() {
--- src/main/java/ru/bmstu/iu3/service/OrderService.java
+++ src/main/java/ru/bmstu/iu3/service/OrderService.java
@@ -6,27 +6,18 @@ import ru.bmstu.iu3.menu.*;
 public class OrderService {
     private final OrderManager order;
     private final MenuRepository menu;
-    private final InputReader reader;
+    private final OrderSelectionFlow selectionFlow;
     private final BillPresenter billPresenter;
 
     public OrderService(OrderManager order, MenuRepository menu, InputReader reader, BillPresenter billPresenter) {
         this.order = order;
         this.menu = menu;
-        this.reader = reader;
+        this.selectionFlow = new OrderSelectionFlow(reader);
         this.billPresenter = billPresenter;
     }
 
     public void makeOrder() {
-        boolean flag = true;
-        System.out.println("------------------------------------------");
-        while(flag)
-            {
-                order.addDish(menu.getDishByNumber(reader.readInt("Выберите номер блюда для добавления в заказ:")));
-                System.out.println();
-                if (reader.readInt("Хотите добавить еще блюдо? 1 - Да, 0 - Нет") == 0) {
-                    flag = false;
-                }
-            }
+        selectionFlow.run(order, menu);
     }
 
     public int getBill() {
